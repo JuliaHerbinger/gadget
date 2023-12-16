b0ed82bb45e44ddf32ba70fff6ba622d034c87c2
Herbinger <Julia.Herbinger@lmu.de>
2023-12-16 13:01:06 +0100

    update file to prepare pint analysis

--- R/simulations/batchtools/reduce_experiments_pint_eval.R
+++ R/simulations/batchtools/reduce_experiments_pint_eval.R
@@ -5,7 +5,7 @@
 reduce_pint = function(ades, pdes, n.repl, problems = NULL, savedir) {
 
 	tab = data.frame(job.id = 1:(nrow(pdes)*nrow(ades)*n.repl))
-	tab$learner = as.character(rep(ades$learner, n.repl*nrow(ades)))
+	tab$learner = as.character(rep(rep(ades$learner, each = n.repl),nrow(pdes)))
 	tab$n = rep(pdes$n, each = n.repl*nrow(ades))
 	tab$type = as.character(rep(pdes$type, each = nrow(ades)*n.repl))
 	tab$dep = as.character(rep(pdes$dep, each = nrow(ades)*n.repl))
@@ -26,7 +26,7 @@ reduce_pint = function(ades, pdes, n.repl, problems = NULL, savedir) {
 			    res.perf = reduceResultsDataTable(toreduce, function(x) x$perf.test)
 			    res.perf = ijoin(tab, res.perf)
 
-				path = file.path(savedir, "result_tables_pint2")
+				path = file.path(savedir, "result_tables_pint")
 
 				if (!dir.exists(path)) 
 				  dir.create(path, recursive = TRUE)
@@ -43,13 +43,13 @@ reduce_pint = function(ades, pdes, n.repl, problems = NULL, savedir) {
 
 library(batchtools)
 reg = loadRegistry("data/batchtools/gadget_sim_pint_test")
-pdes = expand.grid(n = c(300), type = rep(c("spur_pint"), each = 1), stringsAsFactors = FALSE)
+#pdes = expand.grid(n = c(300), type = rep(c("spur_pint"), each = 1), stringsAsFactors = FALSE)
 pdes = expand.grid(n = c(300, 500), type = c("spur_lin", "spur_nonlin"), dep = c("high", "medium"), beta = c(2, 1), noise = c("yes", "no"), stringsAsFactors = FALSE)
 
-ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm"), stringsAsFactors = FALSE)
+#ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm"), stringsAsFactors = FALSE)
 ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm", "regr.ranger"), pint = TRUE, stringsAsFactors = FALSE)
 
-reduce_pint(ades = ades, pdes = pdes,n.repl = 1, savedir = "data/batchtools/")
+reduce_pint(ades = ades, pdes = pdes,n.repl = 1000, savedir = "data/batchtools/")
 
 
 
