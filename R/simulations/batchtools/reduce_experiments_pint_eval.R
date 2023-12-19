1c6ea7c41b9be64656f3ca70d5d226bb5d12b6e9
Herbinger <Julia.Herbinger@lmu.de>
2023-12-19 22:09:44 +0100

    update pint eval

--- R/simulations/batchtools/reduce_experiments_pint_eval.R
+++ R/simulations/batchtools/reduce_experiments_pint_eval.R
@@ -26,7 +26,7 @@ reduce_pint = function(ades, pdes, n.repl, problems = NULL, savedir) {
 			    res.perf = reduceResultsDataTable(toreduce, function(x) x$perf.test)
 			    res.perf = ijoin(tab, res.perf)
 
-				path = file.path(savedir, "result_tables_pint")
+				path = file.path(savedir, "result_tables_pint2")
 
 				if (!dir.exists(path)) 
 				  dir.create(path, recursive = TRUE)
@@ -42,11 +42,13 @@ reduce_pint = function(ades, pdes, n.repl, problems = NULL, savedir) {
 }
 
 library(batchtools)
-reg = loadRegistry("data/batchtools/gadget_sim_pint_eval")
+reg = loadRegistry("data/batchtools/gadget_sim_pint_eval2", writeable = TRUE)
 #pdes = expand.grid(n = c(300), type = rep(c("spur_pint"), each = 1), stringsAsFactors = FALSE)
-pdes = expand.grid(n = c(300, 500), type = c("spur_lin", "spur_nonlin"), dep = c("high", "medium", "no"), beta = c(2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25), noise = c("yes", "no"), stringsAsFactors = FALSE)
+#pdes = expand.grid(n = c(300, 500), type = c("spur_lin", "spur_nonlin"), dep = c("high", "medium", "no"), beta = c(2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25), noise = c("yes", "no"), stringsAsFactors = FALSE)
+pdes = expand.grid(n = c(300, 500), type = c("spur_int"), dep = c("high", "medium", "no"), beta = c(3, 2.75, 2.5, 2.25, 2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25,0), noise = c("yes", "no"))
 
 #ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm"), stringsAsFactors = FALSE)
+#ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm", "regr.ranger"), pint = TRUE, stringsAsFactors = FALSE)
 ades = expand.grid(obj.function = list(list("SS_L2_pd","SS_L2_ale", "SS_L2_shap_rc")), learner = c("regr.ksvm", "regr.ranger"), pint = TRUE, stringsAsFactors = FALSE)
 
 reduce_pint(ades = ades, pdes = pdes,n.repl = 1000, savedir = "data/batchtools/")
